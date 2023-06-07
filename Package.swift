c4b0cfac043aaefa7ed9b76f02540d89084d8cf8
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-07 11:02:29 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["ImageColors"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/ColorSugar", from: "0.0.1"),
+        .package(url: "https://github.com/pxlshpr/ColorSugar", from: "0.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
